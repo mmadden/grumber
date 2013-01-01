5938e6eeeead415efdb177ae83bf1534e81430df
Trek Glowacki <trek@groupon.com>
2012-12-31 20:17:53 -0600

    comment out as-yet unused tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,9 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    uglify: {
-      'dist/built.min.js': 'dist/built.js'
-    },
+    // uglify: {
+    //   'dist/built.min.js': 'dist/built.js'
+    // },
     neuter: {
       'build/application.js': 'app/app.js'
     },
@@ -13,9 +13,9 @@ module.exports = function(grunt) {
       files: ['dependencies/ember.js','app/**/*.js', 'app/**/*.hbs'],
       tasks: ['develop']
     },
-    qunit: {
-      all: ['test/**/*.html']
-    },
+    // qunit: {
+    //   all: ['test/**/*.html']
+    // },
     jshint: {
       all: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js', '!dependencies/*.*'],
       options: {
@@ -35,27 +35,27 @@ module.exports = function(grunt) {
     }
   });
   
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-neuter');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-ember-templates');
   
-  grunt.registerMultiTask('test_runner_file', 'Creates a test runner file.', function(){
-    var tmpl = grunt.file.read('test/support/base.html.tmpl');
-    var renderingContext = {
-      data: {
-        files: this.file.src.map(function(path){
-          return path.replace('test/', '');
-        })
-      }
-    };
-    grunt.file.write('test/base.html', grunt.template.process(tmpl, renderingContext));
-  });
+  // grunt.registerMultiTask('test_runner_file', 'Creates a test runner file.', function(){
+  //   var tmpl = grunt.file.read('test/support/base.html.tmpl');
+  //   var renderingContext = {
+  //     data: {
+  //       files: this.file.src.map(function(path){
+  //         return path.replace('test/', '');
+  //       })
+  //     }
+  //   };
+  //   grunt.file.write('test/base.html', grunt.template.process(tmpl, renderingContext));
+  // });
   
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'neuter', 'test_runner_file', 'qunit']);
+  // grunt.registerTask('default', ['jshint', 'neuter', 'test_runner_file', 'qunit']);
   grunt.registerTask('develop', ['ember_templates', 'neuter']);
 };
