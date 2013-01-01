5e1677bc1e4efbe2c874da4a8f880e290ab6cb78
Trek Glowacki <trek@groupon.com>
2012-12-31 19:23:20 -0600

    remove debugging

--- app/controllers/filtered_todos_controller.js
+++ app/controllers/filtered_todos_controller.js
@@ -1,8 +1,4 @@
 Todos.FilteredTodosController = Ember.ArrayController.extend({
-  init: function(o){
-    this._super(o);
-    window.X = this;
-  },
   toggleTodo: function(event){
     var todo = event.context;
     todo.toggleProperty('completed');
